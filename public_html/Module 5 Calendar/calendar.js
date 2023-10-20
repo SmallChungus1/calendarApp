6adbce89979e927ecb4c92bb505b3a2fc4ed0fe3
Nick <111998485+NicholasEng26@users.noreply.github.com>
2023-10-19 22:16:08 -0500

    Added a Calendar View and a navbar

--- public_html/Module 5 Calendar/calendar.js
+++ public_html/Module 5 Calendar/calendar.js
@@ -100,3 +100,13 @@ const handleDayClick = (day, month, year) => {
         renderCalendarEvents(day, months[month], day, year);
     }
 };
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
